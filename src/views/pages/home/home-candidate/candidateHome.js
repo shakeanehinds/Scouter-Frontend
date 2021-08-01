ba4d2c83d1dbd682f9bb73b57731f891a0775c2d
Lateefah Smellie <lateefah.smellie@gmail.com>
2021-08-01 17:12:43 +0100

    refactor app.js to switch between routes and reander ehader and footer by default.

---
+++ src/views/pages/home/home-candidate/candidateHome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.css';
+///render top level errors, warning and info as banner or modal
+
+export const CandidateHome = () => {
+    return <h1>Candidate Home</h1>;
+}
