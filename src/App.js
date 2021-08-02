f03d111a85815352ea13ec72087f21253c77d6eb
Lateefah Smellie <lateefah.smellie@gmail.com>
2021-08-02 02:51:51 +0100

    minor uwi framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
-import bell from './images/bell.svg';
-import logo from './images/Logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+  /*Link,
+  useRouteMatch,
+  useParams*/
+} from "react-router-dom";
+
+import { AppHeader } from './views/components/AppHeader/index';
+import { AppFooter } from './views/components/AppFooter/index';
+import { Home } from './views/pages/home/home';
 import AdminHome from './views/pages/home-admin/adminHome';
 
+
+
 function App() {
-  return <AdminHome />;
+  //const match = useRouteMatch();
+  return (
+    <div id="App" class="h-screen w-screen">
+      <AppHeader />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/admin">
+            <AdminHome />
+            </Route>
+        </Switch>
+      </Router>
+      <AppFooter />
+    </div>
+  );
 }
 
 export default App;
